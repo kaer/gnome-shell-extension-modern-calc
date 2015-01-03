abd02408fec07b08f270c0eb5c28d9f505dd1342
kaer <kaer@users.noreply.github.com>
2015-01-03 14:01:16 -0200

    new year

--- modern-calc@kaer/module_data/unit_converter/measurement_list.js
+++ modern-calc@kaer/module_data/unit_converter/measurement_list.js
@@ -1,3 +1,29 @@
+/*
+ *    Copyright (C) 2014  Kaer 
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License along
+ *    with this program; if not, write to the Free Software Foundation, Inc.,
+ *    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *    
+ *    Modern Calc 4, Kaer (C) 2014-2015 Kaer
+ *    Modern Calc comes with ABSOLUTELY NO WARRANTY.
+ *
+ *    Author: Kaer (the.thin.king.way+2014@gmail.com)
+ *    Project url: https://github.com/kaer/gnome-shell-extension-modern-calc
+ *
+ */
+
 var MeasurementList = [
         // Length
         {
